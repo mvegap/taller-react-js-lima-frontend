8beee0e6202cdf35bd59c9db154a51d3011c320c
Miguel Vega <mvegaplm@gmail.com>
2020-01-22 12:12:40 -0500

    added posts integration

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,18 +1,73 @@
 import React, { Component } from 'react';
 import { Helmet } from 'react-helmet';
 import Header from './Header';
+import axios from '../utils/axios';
 
 class Blog extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoaded: false,
+      error: null,
+      posts: []
+    }
+  }
+  async componentDidMount() {
+    try {
+      let arrData = await axios.get('/posts');
+      arrData = arrData.data;
+      this.setState({
+        isLoaded: true,
+        posts: arrData,
+      });
+    } catch (e) {
+      this.setState({
+        isLoaded: true,
+        error: e,
+      });
+      console.log(`😱 Error al realizar la petición en AXIOS: ${e}`);
+    }
+  }
   render() {
-    return(
-      <React.Fragment>
-        <Helmet>
-          <title>Blog | Lima Frontend</title>
-        </Helmet>
-        <Header />
-        <h1>Last news</h1>
-      </React.Fragment>
-    );
+    const { error, isLoaded, posts } = this.state;
+    if (error) {
+      return(
+        <React.Fragment>
+          <Helmet>
+            <title>Error: {error.message}</title>
+          </Helmet>
+          <Header />
+          <h1>Last news</h1>
+          <p>Hubo un error: {error.message}</p>
+        </React.Fragment>
+      );
+    } else if (!isLoaded) {
+      return (
+        <React.Fragment>
+          <Helmet>
+            <title>Cargando...</title>
+          </Helmet>
+          <Header />
+          <h1>Last news</h1>
+          <p>Cargando datos...</p>
+        </React.Fragment>
+      );
+    }else {
+      return(
+        <React.Fragment>
+          <Helmet>
+            <title>Blog | Lima Frontend</title>
+          </Helmet>
+          <Header />
+          <h1>Last news</h1>
+          <ul>
+            { posts.map(item => (
+              <li key={item.id}>{item.title}</li>
+            )) }
+          </ul>
+        </React.Fragment>
+      );
+    }
   }
 };
 
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -11,7 +11,7 @@ class Header extends Component {
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/portfolio">Portfolio</Link></li>
-          <li><Link to="/blog">Blog</Link></li>
+          <li><Link to="/blog" replace>Blog</Link></li>
           <li><Link to="/contact">Contact</Link></li>
         </ul>
       </div>
